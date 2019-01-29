ac680008844ecab7e8378b9435a6bd72f54e8a10
laykith <laykith@gmail.com>
2019-01-29 09:27:03 +0100

    Added auth middlewares

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,6 +8,10 @@ use App\Participant;
 
 class MessageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     /**
      * Store a newly created resource in storage.
      *
